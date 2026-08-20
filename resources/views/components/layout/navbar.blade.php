706fd081c054fa9246eed3a8c942c7a00a769e8c
odqz <178620777+odqz@users.noreply.github.com>
2026-08-20 22:49:11 +0100

    Added navbar with links to all the major features

--- resources/views/components/layout/navbar.blade.php
+++ resources/views/components/layout/navbar.blade.php
@@ -1,3 +1,30 @@
-<div>
-    
+<div class="navbar">
+  <div class="left">
+    <a href="" class="title">
+      <img src="{{asset('favicon.ico')}}" alt="Site logo">
+      <h1>Library</h1>
+    </a>
+    <a href="">anime</a>
+    <p>|</p>
+    <a href="">manga</a>
+    <p>|</p>
+    <a href="">community</a>
+    <p>|</p>
+    <a href="">help</a>
+    <p>|</p>
+    <a href="">search</a>
+  </div>
+  <div class="right">
+    @if(Auth::check())
+      <a href="">profile</a>
+      <p>|</p>
+      <a href="">account</a>
+      <p>|</p>
+      <a href="">logout</a>
+    @else
+      <a href="/login">login</a>
+      <p>|</p>
+      <a href="/register">register</a>
+    @endif
+  </div>
 </div>
