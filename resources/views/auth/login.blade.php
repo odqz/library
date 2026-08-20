dcf1b20a29ac2dd43179844e6260dad5da8c453c
odqz <178620777+odqz@users.noreply.github.com>
2026-08-20 22:47:45 +0100

    Removed unesscassary tailwind classes from elements

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,36 +1,18 @@
 <x-layout>
-  <form action="/login" method="post" class="rounded p-8">
+  <form action="/login" method="post" class="login">
     @csrf
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-        Email
-      </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-        id="email"
-        type="email"
-        name="email"
-        max="255"
-        placeholder="johndoe@example.com"
-        required>
+    <div class="fields">
+      <div class="field">
+        <label for="email">Email</label>
+        <input  id="email" type="email" name="email" max="255" placeholder="johndoe@example.com" required>
+      </div>
+      <div class="field">
+        <label for="password">Password</label>
+        <input id="password" type="password" name="password" min="8" max="255" placeholder="**************" required>
+      </div>
     </div>
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-        id="password" 
-        type="password"
-        name="password"
-        min="8"
-        max="255"
-        placeholder="**********"
-        required>
-    </div>
-    <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-        Sign In
-      </button>
-      <!-- <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
-      </a> -->
+    <div class="action">
+      <button type="submit">Log In</button>
     </div>
   </form>
 </x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,47 +1,23 @@
 <x-layout.layout>
-  <form action="/register" method="post" class="rounded p-8">
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-        Username
-      </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="name" 
-        type="text"
-        name="name"
-        max="255"
-        placeholder="John Doe"
-        required>
+  <form action="/register" method="post" class="register">
+    <div class="fields">
+      <div class="field">
+        <label for="name">Name</label>
+        <input id="name" type="text" name="name" max="255" placeholder="John Doe" required>
+      </div>
+      <div class="field">
+        <label for="email">
+          Email
+        </label>
+        <input id="email" type="email" name="email" max="255" placeholder="johndoe@example.com" required>
+      </div>
+      <div class="field">
+        <label for="password">Password</label>
+        <input id="password" type="password" name="password" min="8" max="255" placeholder="**************" required>
+      </div>
     </div>
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-        Email
-      </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-        id="email"
-        type="email"
-        name="email"
-        max="255"
-        placeholder="johndoe@example.com"
-        required>
-    </div>
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-        id="password" 
-        type="password"
-        name="password"
-        min="8"
-        max="255"
-        placeholder="**********"
-        required>
-    </div>
-    <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-        Sign In
-      </button>
-      <!-- <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
-      </a> -->
+    <div class="action">
+      <button type="submit">Create account</button>
     </div>
   </form>
 </x-layout.layout>
