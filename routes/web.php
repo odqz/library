3c2a82df9e8425c8bceb003100724310e29b6cf9
odqz <178620777+odqz@users.noreply.github.com>
2026-08-21 21:49:33 +0100

    Added logout route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\SessionsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::get('/', function () { return view('welcome'); });
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::delete('/logout', [SessionsController::class, 'destroy']);
 });
 
 Route::middleware('guest')->group(function () {
