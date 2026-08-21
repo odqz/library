1ce7f0f53542c4ae82716f74943cd16dbfdb9683
odqz <178620777+odqz@users.noreply.github.com>
2026-08-21 21:49:00 +0100

    Added footer

---
+++ resources/views/components/layout/sidebar.blade.php
@@ -0,0 +1,14 @@
+<div class="sidebar">
+  <div class="top">
+    <a href="">rules</a>
+    <p>|</p>
+    <a href="">faq</a>
+    <p>|</p>
+    <a href="">about</a>
+    <p>|</p>
+    <a href="">repo</a>
+  </div>
+  <a href="" class="bottom">
+      <img src="{{asset('favicon.ico')}}" alt="Site logo">
+    </a>
+</div>
