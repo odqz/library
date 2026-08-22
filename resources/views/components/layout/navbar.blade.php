76886f3f383c57b5075d0ced4f39c2a4451d9b7a
odqz <178620777+odqz@users.noreply.github.com>
2026-08-22 21:15:23 +0100

    Added my shelf link, home link and account link

--- resources/views/components/layout/navbar.blade.php
+++ resources/views/components/layout/navbar.blade.php
@@ -1,6 +1,6 @@
 <div class="navbar">
   <div class="left">
-    <a href="" class="title">
+    <a href="/" class="title">
       <img src="{{asset('favicon.ico')}}" alt="Site logo">
       <h1>Library</h1>
     </a>
@@ -9,14 +9,14 @@
     <a href="">manga</a>
     <p>|</p>
     <a href="">community</a>
-    <p>|</p>
-    <a href="">help</a>
-    <p>|</p>
-    <a href="">search</a>
+    @if(Auth::check())
+      <p>|</p>
+      <a href="/users/">my shelf</a>
+    @endif
   </div>
   <div class="right">
     @if(Auth::check())
-      <a href="">account</a>
+      <a href="/users/{{Auth::user()->id}}">{{Auth::user()->name}}</a>
       <p>|</p>
       <form action="/logout" method="post">
         @csrf
