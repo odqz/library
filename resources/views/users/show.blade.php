e94463dda0330a9af15aae68240ee5eaa4df7ce6
odqz <178620777+odqz@users.noreply.github.com>
2026-08-24 21:54:45 +0100

    Added required info including new password, delete account and account info to show user page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,3 +1,34 @@
 <x-layout.layout>
-  <p>sup</p>
+  <div class="account-info">
+    <div class="account-name">Username: {{ $user->username }}</div>
+    <div class="account-created">Joined {{ $user->created_at }}</div>
+
+    <form action="" method="post">
+      <div class="field">
+        <label for="">Current password</label>
+        <input type="text">
+      </div>
+        
+      <div class="field">
+        <label for="">New password</label>
+        <input type="text">
+      </div>
+
+      <div class="field">
+        <label for="">Confirm new password</label>
+        <input type="text">
+      </div>
+
+      <div class="action">
+        <button type="submit">Change password</button>
+      </div>
+    </form>
+
+    <form action="" method="post">
+      <div class="action">
+        <button type="submit">Delete account</button>
+      </div>
+    </form>
+    
+  </div>
 </x-layout.layout>
