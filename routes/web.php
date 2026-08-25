ee960541084b95922be72ac8b9b1f670f72b6b20
odqz <178620777+odqz@users.noreply.github.com>
2026-08-25 22:00:18 +0100

    Added one route for login page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\SessionsController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -9,11 +8,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/', function () { return view('welcome'); });
     Route::get('/users/{user}', [UserController::class, 'show']);
     Route::delete('/logout', [SessionsController::class, 'destroy']);
+    Route::delete('/delete-account', [UserController::class, 'destroy']);
 });
 
 Route::middleware('guest')->group(function () {
-    Route::get('/register', [RegisteredUserController::class, 'create']);
-    Route::post('/register', [RegisteredUserController::class, 'store']);
-    Route::get('/login', [SessionsController::class, 'create']);
+    Route::get('/login', function () { return view('auth.login'); });
     Route::post('/login', [SessionsController::class, 'store']);
+    Route::post('/create-account', [UserController::class, 'store']);
 });
