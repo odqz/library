0ff1734e160a41470273a3bf35aceaa2379e146f
odqz <178620777+odqz@users.noreply.github.com>
2026-08-25 22:00:41 +0100

    Created show page and added js script which shows/hides change password form

---
+++ resources/js/change-password.js
@@ -0,0 +1,4 @@
+document.querySelector('#change-password-btn').addEventListener('click', () => {
+  document.querySelector('#change-password-form').classList.toggle('hidden');
+  document.querySelector('#change-password-form').classList.toggle('flex');
+});
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,34 +1,28 @@
+@vite(['resources/js/change-password.js']);
+
 <x-layout.layout>
-  <div class="account-info">
-    <div class="account-name">Username: {{ $user->username }}</div>
-    <div class="account-created">Joined {{ $user->created_at }}</div>
-
-    <form action="" method="post">
-      <div class="field">
-        <label for="">Current password</label>
-        <input type="text">
-      </div>
-        
-      <div class="field">
-        <label for="">New password</label>
-        <input type="text">
-      </div>
-
-      <div class="field">
-        <label for="">Confirm new password</label>
-        <input type="text">
-      </div>
-
-      <div class="action">
-        <button type="submit">Change password</button>
-      </div>
+  <div class="flex flex-col gap-1">
+    <div>
+      <span class="text-[#4a4a4a]">username: </span>
+      <span class="text-(--logo-blue) font-bold">{{ $user->username }}</span>
+    </div>
+    <div>
+      <span class="text-[#4a4a4a]">created: </span>
+      <span class="text-(--logo-blue) font-bold">{{ $user->created_at->format('d/m/y') }}</span>
+    </div>
+    <div>
+      <button id="change-password-btn" class="underline cursor-pointer">Change password</button>
+    </div>
+    <form action="/update" method="post" class="hidden flex-col items-start gap-2 p-2 border" id="change-password-form">
+      <x-auth.field label="Current password" name="current-password" type="password" min="8" max="255" placeholder="************" required></x-auth.field>
+      <x-auth.field label="New password" name="new-password" type="password" min="8" max="255" placeholder="************" required></x-auth.field>
+      <x-auth.field label="Confirm password" name="confirm-password" type="password" min="8" max="255" placeholder="************" required></x-auth.field>
+      <button class="bg-(--good-green) py-0.5 px-3 text-(--text-bright-white) hover:brightness-[94%]" type="submit">Change password</button>
     </form>
-
-    <form action="" method="post">
-      <div class="action">
-        <button type="submit">Delete account</button>
-      </div>
+    <form action="/delete-account" method="post" class="my-2">
+      @csrf
+      @method('DELETE')
+      <button class="bg-(--bad-red) py-0.5 px-3 text-(--text-bright-white) hover:brightness-[94%]" type="submit">Delete account</button>
     </form>
-    
   </div>
 </x-layout.layout>
