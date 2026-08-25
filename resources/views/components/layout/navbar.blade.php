6563d0147fdf14a9a59a1dc4d99845da1276e052
odqz <178620777+odqz@users.noreply.github.com>
2026-08-25 21:55:49 +0100

    Turned basic css into tailwind classes

--- resources/views/components/layout/navbar.blade.php
+++ resources/views/components/layout/navbar.blade.php
@@ -1,28 +1,28 @@
-<div class="navbar">
-  <div class="left">
-    <a href="/" class="title">
-      <img src="{{asset('favicon.ico')}}" alt="Site logo">
-      <h1>Library</h1>
+<div class="flex items-center justify-between w-[80%] bg-(--dark-blue) mt-2 text-(--text-white) py-0.5 px-1 pr-2 text-sm">
+  <div class="flex items-end gap-1">
+    <a href="/" class="flex items-center">
+      <img src="{{asset('logo.png')}}" alt="Site logo" class="w-8">
+      <h1 class="text-[#e9e9e9] font-bold">Library</h1>
     </a>
-    <a href="">anime</a>
-    <p>|</p>
-    <a href="">manga</a>
-    <p>|</p>
-    <a href="/users/">my shelf</a>
+    <div class="flex">
+      <a href="/animes/">anime</a>
+      <p>|</p>
+      <a href="/mangas/">manga</a>
+      <p>|</p>
+      <a href="/users/">my shelf</a>
+    </div>
   </div>
-  <div class="right">
+  <div class="flex item-center">
     @if(Auth::check())
       <a href="/users/{{Auth::user()->id}}">{{Auth::user()->username}}</a>
       <p>|</p>
       <form action="/logout" method="post">
         @csrf
         @method('DELETE')
-        <button type="submit">logout</button>
+        <button class="hover:cursor-pointer" type="submit">logout</button>
       </form>
     @else
       <a href="/login">login</a>
-      <p>|</p>
-      <a href="/register">register</a>
     @endif
   </div>
 </div>
