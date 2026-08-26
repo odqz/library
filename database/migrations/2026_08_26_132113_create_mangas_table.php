5c32e87e55b48147679b1c1d3056ea6a6c36c097
odqz <178620777+odqz@users.noreply.github.com>
2026-08-26 21:46:55 +0100

    Created mangas table

---
+++ database/migrations/2026_08_26_132113_create_mangas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mangas', function (Blueprint $table) {
+            $table->id();
+            $table->string('title_english')->nullable();
+            $table->string('title_romaji')->nullable();
+            $table->integer('score')->nullable();
+            $table->integer('chapters')->nullable();
+            $table->integer('volumes')->nullable();
+            $table->string('status')->nullable();
+            $table->text('description')->nullable();
+            $table->string('cover_image_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mangas');
+    }
+};
