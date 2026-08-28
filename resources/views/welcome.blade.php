0d6d2100f9187485afad380739df416bbd0ebb91
odqz <178620777+odqz@users.noreply.github.com>
2026-08-28 21:19:24 +0100

    Making notes on welcome page for how the site should function

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,11 @@
 <x-layout.layout>
     <div class="flex flex-col">
-        <p>Yo, you are on the welcome page</p>
-        <p>You should only see this if you are logged in</p>
+        <p>Yo, so this is the plan</p>
+        <p>Im gonna keep it simple first and just do manga then do anime</p>
+        <p>- This page will have a users currently reading with the links to reading entry and the manga</p>
+        <p>- The consumption will be the ReadingController#edit page</p>
+        <p>- The manga will be the MangaController#show page</p>
+        <p>- The shelf page will be the ReadingController#index page i think?</p>
+        <p>Also need to change account page to edit method not show</p>
     </div>
 </x-layout.layout>
