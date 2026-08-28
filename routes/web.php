bc8870130569cbfb7b9ca64ec64c2b4f42652dae
odqz <178620777+odqz@users.noreply.github.com>
2026-08-28 17:10:57 +0100

    Created route to animes index page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnimeController;
 use App\Http\Controllers\Auth\SessionsController;
 use App\Http\Controllers\MangaController;
 use App\Http\Controllers\UserController;
@@ -8,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::get('/', function () { return view('welcome'); });
     Route::get('/mangas/', [MangaController::class, 'index']);
+    Route::get('/animes/', [AnimeController::class, 'index']);
     Route::get('/users/{user}', [UserController::class, 'show']);
     Route::delete('/logout', [SessionsController::class, 'destroy']);
     Route::delete('/delete-account', [UserController::class, 'destroy']);
