21b18b5b1d5a697bdf141febc4d9475fe8360e68
odqz <178620777+odqz@users.noreply.github.com>
2026-09-11 18:50:08 +0100

    fix: Foregin id error which referenced users Now there are two foreign ids, one for anime and one for manga

--- database/migrations/2026_09_08_123609_create_staff_table.php
+++ database/migrations/2026_09_08_123609_create_staff_table.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Models\User;
+use App\Models\Anime;
+use App\Models\Manga;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('staff', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Manga::class)->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Anime::class)->nullable()->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->string('role');
             $table->string('image_path');
