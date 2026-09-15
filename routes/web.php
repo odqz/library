8712290af639eafdf2d04420e169305c56617bba
odqz <178620777+odqz@users.noreply.github.com>
2026-09-15 21:42:06 +0100

    feat: Added routes for create/edit/delete for watchings/reading

--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,29 @@
 use App\Http\Controllers\AnimeController;
 use App\Http\Controllers\Auth\SessionsController;
 use App\Http\Controllers\MangaController;
+use App\Http\Controllers\ReadingController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WatchingController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::get('/', function () { return view('welcome'); });
 
-    // View all animes and view individual animes
-    Route::get('/animes/', [AnimeController::class, 'index']);
-    Route::get('/animes/{anime}', [AnimeController::class, 'show']);
-
-    // View all manga and view individual manga
-    Route::get('/mangas/', [MangaController::class, 'index']);
-    Route::get('/mangas/{manga}', [MangaController::class, 'show']);
-
-    // View user edit page, user shelf and accept delete account
     Route::get('/users/{user}', [UserController::class, 'show']);
     Route::get('/users/{user}/edit', [UserController::class, 'edit']);
-    Route::delete('/delete-account', [UserController::class, 'destroy']);
 
+    Route::get('/readings/new/{manga}', [ReadingController::class, 'create']);
+    Route::post('/readings/{manga}', [ReadingController::class, 'store']);
+    Route::get('/readings/{manga}', [ReadingController::class, 'show']);
+    Route::delete('/readings/{reading}', [ReadingController::class, 'destroy']);
+
+    Route::get('/watchings/new/{anime}', [WatchingController::class, 'create']);
+    Route::post('/watchings/{anime}', [WatchingController::class, 'store']);
+    Route::get('/watchings/{watching}/edit', [WatchingController::class, 'edit']);
+    Route::patch('/watchings/{watching}', [WatchingController::class, 'update']);
+    Route::delete('/watchings/{watching}', [WatchingController::class, 'destroy']);
+
+    Route::delete('/delete-account', [UserController::class, 'destroy']);
     Route::delete('/logout', [SessionsController::class, 'destroy']);
 });
 
@@ -30,3 +34,9 @@ Route::middleware('guest')->group(function () {
     Route::post('/login', [SessionsController::class, 'store']);
     Route::post('/create-account', [UserController::class, 'store']);
 });
+
+Route::get('/animes/', [AnimeController::class, 'index']);
+Route::get('/animes/{anime}', [AnimeController::class, 'show']);
+
+Route::get('/mangas/', [MangaController::class, 'index']);
+Route::get('/mangas/{manga}', [MangaController::class, 'show']);
