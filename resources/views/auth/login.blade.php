b614955b66fa5cb5274be0b7418a7b4774732b23
odqz <178620777+odqz@users.noreply.github.com>
2026-09-16 22:30:23 +0100

    feat: Switched from old field component to new

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,14 +2,22 @@
   <div class="flex flex-col gap-6">
     <form action="/login" method="post" class="flex flex-col items-start gap-2">
       @csrf
-      <x-auth.field name="username" max="255" placeholder="Somebody42" required></x-auth.field>
-      <x-auth.field name="password" type="password" min="8" max="255" placeholder="************" required></x-auth.field>
-      <button class="bg-(--logo-blue) py-0.5 px-3 text-(--text-bright-white) hover:brightness-[94%]" type="submit">Log in</button>
+      <div class="grid grid-cols-[80px_1fr] gap-2">
+        <x-form.field name="username" label="Username" type="text" max="255" placeholder="somebody42" required></x-form.field>
+      </div>
+      <div class="grid grid-cols-[80px_1fr] gap-2">
+        <x-form.field name="password" label="Password" type="password" max="255" min="8" placeholder="**********" required></x-form.field>
+      </div>
+      <button class="bg-(--logo-blue) py-0.5 px-3 text-(--text-bright-white) hover:brightness-[94%]" type="submit">Login</button>
     </form>
     <form action="/create-account" method="post" class="flex flex-col items-start gap-2">
       @csrf
-      <x-auth.field name="username" max="255" placeholder="Somebody42" required></x-auth.field>
-      <x-auth.field name="password" type="password" min="8" max="255" placeholder="************" required></x-auth.field>
+      <div class="grid grid-cols-[80px_1fr] gap-2">
+        <x-form.field name="username" label="Username" type="text" max="255" placeholder="somebody42" required></x-form.field>
+      </div>
+      <div class="grid grid-cols-[80px_1fr] gap-2">
+        <x-form.field name="password" label="Password" type="password" max="255" min="8" placeholder="**********" required></x-form.field>
+      </div>
       <button class="bg-(--good-green) py-0.5 px-3 text-(--text-bright-white) hover:brightness-[94%]" type="submit">Create account</button>
     </form>
   </div>
