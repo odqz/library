07ec56ebe65ea9874af4e42e6c944006a36c2ccd
odqz <178620777+odqz@users.noreply.github.com>
2026-09-18 09:00:16 +0100

    fix: Swapped from old fields to new ones

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -10,14 +10,22 @@
       <span class="text-[#4a4a4a]">created: </span>
       <span class="text-(--logo-blue) font-bold">{{ $user->created_at->format('d/m/y') }}</span>
     </div>
-    <div>
-      <button id="change-password-btn" class="underline cursor-pointer">Change password</button>
-    </div>
-    <form action="/update" method="post" class="hidden flex-col items-start gap-2 p-2 border" id="change-password-form">
-      <x-auth.field label="Current password" name="current-password" type="password" min="8" max="255" placeholder="************" required></x-auth.field>
-      <x-auth.field label="New password" name="new-password" type="password" min="8" max="255" placeholder="************" required></x-auth.field>
-      <x-auth.field label="Confirm password" name="confirm-password" type="password" min="8" max="255" placeholder="************" required></x-auth.field>
-      <button class="bg-(--good-green) py-0.5 px-3 text-(--text-bright-white) hover:brightness-[94%]" type="submit">Change password</button>
+    <form action="/users/{{ $user->id }}" method="post" class="flex flex-col items-start gap-1 p-2 border">
+      <button id="change-password-btn" class="cursor-pointer w-[100%]" type="button">Change password</button>
+      @csrf
+      @method('PATCH')
+      <div class="item hidden flex flex-col">
+        <x-form.field name="current-password" label="Current password" type="password" min="8" max="255" placeholder="**********" required></x-form.field>
+      </div>
+      <div class="item hidden flex flex-col">
+        <x-form.field name="new-password" label="New password" type="password" min="8" max="255" placeholder="**********" required></x-form.field>
+      </div>
+      <div class="item hidden flex flex-col">
+        <x-form.field name="confirm-new-password" label="Confirm password" type="password" min="8" max="255" placeholder="**********" required></x-form.field>
+      </div>
+      <div class="item hidden">
+        <button class="bg-(--good-green) mt-1 py-0.5 px-3 text-(--text-bright-white) hover:brightness-[94%]" type="submit">Change password</button>
+      </div>
     </form>
     <form action="/delete-account" method="post" class="my-2">
       @csrf
