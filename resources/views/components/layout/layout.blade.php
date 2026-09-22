383cd44679e4c4dcb464111c431cb0c2981cc92d
odqz <178620777+odqz@users.noreply.github.com>
2026-09-22 15:35:12 +0100

    fix: Styling issue in layout page

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body class="flex flex-col items-center">
     <x-layout.navbar></x-layout.navbar>
-    <div class="content flex bg-(--background-white) w-[80%] p-2">
+    <div class="flex-col bg-(--background-white) w-[80%] p-2">
         {{ $slot }}
     </div>
     <x-layout.footer></x-layout.footer>
